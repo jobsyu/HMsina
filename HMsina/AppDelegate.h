dc1777cfbab1bee804ebffbebf999f70fd707a52
jobsyu <yuchengpeng@yuchengpengdeMac.local>
2015-06-25 15:55:30 +0800

    Initial Commit

---
+++ HMsina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMsina
+//
+//  Created by 於程鹏 on 15/6/25.
+//  Copyright (c) 2015年 於程鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
